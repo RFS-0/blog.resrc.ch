43c00ca92da8f9a0480e19ba17351a522c9f132c
rfs <remo@mountain-yard.ch>
2023-05-11 13:34:44 +0200

    dev-config: add solid devtools

--- blog/vite.config.ts
+++ blog/vite.config.ts
@@ -1,11 +1,16 @@
 import vercel from 'solid-start-vercel';
 import solid from 'solid-start/vite';
+import devtools from 'solid-devtools/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
-      solid({
-        ssr: false,
-        adapter: vercel({})
-      })],
+    devtools({
+          autoname: true,
+        },
+    ),
+    solid({
+      ssr: false,
+      adapter: vercel({}),
+    })],
 });
