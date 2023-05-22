f3a9ce6d113dec33f983092d91a20b2fe73ec4cd
rfs <remo@mountain-yard.ch>
2023-05-22 10:18:55 +0200

    feat: align preview size of posts by only showing the first sentence in small preview mode

--- blog/src/components/post/abstract/Abstract.tsx
+++ blog/src/components/post/abstract/Abstract.tsx
@@ -1,19 +1,26 @@
-import { Component, ParentProps } from 'solid-js';
-import { usePostContext } from '~/components/post/PostContextProvider';
+import {FlowComponent} from 'solid-js';
+import {usePostContext} from '~/components/post/PostContextProvider';
 import './abstract-styles.css';
 
-export type AbstractProps = {} & ParentProps
+export type AbstractProps = {}
 
-export const Abstract: Component<AbstractProps> = props => {
-  const {displaySignal} = usePostContext();
-  const [display] = displaySignal;
-  return (
-      <span
-          class="abstract"
-          classList={{
-            'body-small': display() === 'preview-small',
-            'body-large': display() === 'preview-large',
-            'title-small': display() === 'full',
-          }}>{props.children}</span>
-  );
+export const Abstract: FlowComponent<AbstractProps, string> = props => {
+    const {displaySignal} = usePostContext();
+    const [display] = displaySignal;
+
+    console.log(props.children)
+
+    const firstSentence = props.children.split('.')[0]
+
+    return (
+        <span
+            class="abstract"
+            classList={{
+                'body-small': display() === 'preview-small',
+                'body-large': display() === 'preview-large',
+                'title-small': display() === 'full',
+            }}>
+            {display() === 'preview-small' ? firstSentence + '.' : props.children}
+      </span>
+    );
 };
