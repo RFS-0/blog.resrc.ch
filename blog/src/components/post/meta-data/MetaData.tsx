ccf61b16e0294b4dae033d8dd6b52ca260948c61
rfs <remo@mountain-yard.ch>
2023-05-22 14:04:04 +0200

    fix: styling of meta data

--- blog/src/components/post/meta-data/MetaData.tsx
+++ blog/src/components/post/meta-data/MetaData.tsx
@@ -1,31 +1,26 @@
-import { Component, For } from 'solid-js';
-import { DatePublished } from '~/components/post/date-published/DatePublished';
-import { Tag } from '~/components/post/tag/Tag';
-import { usePostContext } from '~/components/post/PostContextProvider';
+import {Component, For} from 'solid-js';
+import {DatePublished} from '~/components/post/date-published/DatePublished';
+import {Tag} from '~/components/post/tag/Tag';
+import './meta-data-styles.css';
 
 export type MetaData = {
-  publishedOn: Date,
-  tags: string[]
+    publishedOn: Date,
+    tags: string[]
 }
 
 export const MetaData: Component<MetaData> = (props) => {
-  const {displaySignal} = usePostContext();
-  const [display, setDisplay] = displaySignal;
-
-  return (
-      <div>
-        <div id={'post-meta-data'} class={'flex flex-row flex-wrap gap-f4'}>
-          <DatePublished
-              date={props.publishedOn}
-          />
-          <For each={props.tags}>
-            {(tag) => (
-                <Tag
-                    tag={tag}
-                />
-            )}
-          </For>
+    return (
+        <div class='meta-data'>
+            <DatePublished
+                date={props.publishedOn}
+            />
+            <For each={props.tags}>
+                {(tag) => (
+                    <Tag
+                        tag={tag}
+                    />
+                )}
+            </For>
         </div>
-      </div>
-  );
+    );
 };
