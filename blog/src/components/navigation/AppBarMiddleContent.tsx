dfecf180db89286963d4a4d600da5108c3a86572
rfs <remo@mountain-yard.ch>
2023-05-23 09:10:14 +0200

    feat: improve proportions of top app bare

--- blog/src/components/navigation/AppBarMiddleContent.tsx
+++ blog/src/components/navigation/AppBarMiddleContent.tsx
@@ -1,18 +1,17 @@
-import { Component } from 'solid-js';
+import {Component} from 'solid-js';
 import './top-app-bar-middle-content-styles.css';
 
-export type AppBarMiddleContentProps = {
-}
+export type AppBarMiddleContentProps = {}
 
 export const AppBarMiddleContent: Component<AppBarMiddleContentProps> = (_) => {
 
-  return (
-    <div class='app-bar-middle-content'>
-      <div class='app-bar-middle-content__title body-large'>
-        resrc Blog
-      </div>
-      <div class='app-bar-middle-content__search'>
-      </div>
-    </div>
-  );
+    return (
+        <div class='app-bar-middle-content'>
+            <div class='app-bar-middle-content__title blog-title'>
+                resrc Blog
+            </div>
+            <div class='app-bar-middle-content__search'>
+            </div>
+        </div>
+    );
 }
